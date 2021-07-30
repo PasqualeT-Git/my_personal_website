e0fea824f66438fc6dcc5c5ee5d821743bb5e6a2
Pasquale Troise <troisepasquale1990@gmail.com>
2021-07-30 10:31:30 +0100

    Add functionalities to the website

---
+++ javascript/functions/responsiveBanner.js
@@ -0,0 +1,18 @@
+function responsiveBanner() {
+  // const bannerHeader = document.querySelector('.banner--header');
+  // const banner = document.querySelector('.banner');
+  // const aspectRatio = window.innerWidth / window.innerHeight;
+
+  // console.log(aspectRatio)
+  // // if(aspectRatio < 1.8) {
+  // //   banner.style.height = `calc(100vh - (${bannerHeader.offsetWidth}px - 150px))`;
+  // // }
+
+  // if (WURFL.is_mobile && aspectRatio < 0.80) {
+  //   banner.style.height = `calc(100vh - (${bannerHeader.offsetWidth}px - 100px))`;
+  // }
+
+  // console.log(WURFL.is_mobile);
+}
+
+export { responsiveBanner }
---
+++ javascript/functions/scrollSmoothTo.js
@@ -0,0 +1,19 @@
+function scrollSmoothTo() {
+  // const skills = document.querySelector('#skills-button');
+  // const projects = document.querySelector('#projects-button');
+  // const about = document.querySelector('#about-button');
+  // const contact = document.querySelector('#contact-button');
+
+  // function scroll(button, y) {
+  //   button.addEventListener('click', () => {
+  //     window.scroll(0, y)
+  //   });
+  // }
+
+  // scroll(skills, 600);
+  // scroll(projects, 1130);
+  // scroll(about, 1130);
+  // scroll(contact, 1130);
+}
+
+export {scrollSmoothTo}
